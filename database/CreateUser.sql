947e846245f020ff22475e7847ca15d95cebd2aa
Mike Kvintus <mike.kvintus@mkvintus-mbp-302.local>
2013-07-20 10:04:39 -0400

    Changed database to aha instead of Aha so scripts would work correctly.

--- database/CreateUser.sql
+++ database/CreateUser.sql
@@ -1,2 +1,2 @@
-CREATE USER `ahauser` IDENTIFIED BY 'hahafunny1!';
-GRANT ALL PRIVILEGES ON `Aha` . * TO 'ahauser'@'localhost' WITH GRANT OPTION ;
+GRANT ALL PRIVILEGES ON `aha`.* TO 'ahauser'@'localhost' IDENTIFIED BY 'hahafunny1!' WITH GRANT OPTION ;
+GRANT ALL PRIVILEGES ON `aha`.* TO 'ahauser' IDENTIFIED BY 'hahafunny1!' WITH GRANT OPTION ;
