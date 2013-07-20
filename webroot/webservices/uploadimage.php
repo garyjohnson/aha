557bdfe42735adaa6b52ae84ae44f4b990ff8834
davelhs03 <davelhs03@yahoo.com>
2013-07-20 12:12:35 -0400

    Added check for blacklisted device.  Created insert queries for device and image

--- webroot/webservices/uploadimage.php
+++ webroot/webservices/uploadimage.php
@@ -1,6 +1,7 @@
 <?php
 // Take image save to file system under images and then insert into device folder 
 // and save the device to the database and the image.
+require_once '../source/AHAConstants.php';
 require_once '../source/config.php';
 require_once('../source/MySQLi.class.php');
 
@@ -16,6 +17,33 @@ file_put_contents($file, $current.  date("F j, Y, g:i a")."\n"
         ."==GetAllHeaders==".print_r(getallheaders(), TRUE)."\n");
 
 
+// -------------------------------------------
+// - Save metadata to the database
+// -------------------------------------------
+
+//::TODO:: use config file for DB connection
+//$db = new SQL(Config::$mysql);
+$db = new SQL($host = "localhost", $user = "ahauser", $pass = "hahafunny1!", $db = "aha", $prefix = "", $debug = FALSE);
+
+
+
+//check if device is black listed before saving file an
+
+$resultSet = $db->select('device', '*', 'WHERE guid='.$_REQUEST['device']);
+if($row = $db->fetch())
+{
+    if ($row['blacklisted']== AHAConstants::DEVICE_BLACKLISTED)
+        exit();
+    
+}
+else 
+    {
+    //insert device into databaes
+    $db->insert('devices', array('guid'=>$_REQUEST['device'],'email'=>$_REQUEST['email']));
+}
+
+
+$db->insert('images', array('guid'=>$_REQUEST['device']."_".$_FILES["file"]["name"],'device'=>$_REQUEST['device'],'status'=>'0'));
 
 // -------------------------------------------
 // - Saves file to filesystem at ../images/
@@ -31,17 +59,5 @@ else
     echo "Stored in: " . "../images/" . $_REQUEST['device'] . "_" . $_FILES["file"]["name"];
 }
 
-// -------------------------------------------
-// - Save metadata to the database
-// -------------------------------------------
-
-//::TODO:: use config file for DB connection
-//$db = new SQL(Config::$mysql);
-$db = new SQL($host = "localhost", $user = "ahauser", $pass = "hahafunny1!", $db = "aha", $prefix = "", $debug = FALSE);
-
-//check if device is black listed before saving file an
-$db->selec
-//$db->insert('devices', '')
-$db->insert('images', array('guid'=>$_REQUEST['device'].$_FILES["file"]["name"],'device'=>$_REQUEST['device'],'status'=>'0'));
 ?>
 
