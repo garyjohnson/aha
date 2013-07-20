f2989707568f0aebddf5f9bb5cd8a511b7000393
Gary Johnson <gary@gjtt.com>
2013-07-20 14:30:04 -0400

    Image Picker saving to docs and using upload manager

--- mobile/ios/AHAcleveland/AHAcleveland/AppDelegate.h
+++ mobile/ios/AHAcleveland/AHAcleveland/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  AHAcleveland
-//
-//  Created by Bill Davis on 7/19/13.
-//  Copyright (c) 2013 Bill Davis. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
