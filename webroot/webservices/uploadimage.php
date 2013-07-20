cf165a0cb7c1acc6815c7701611715a6fe514bf0
davelhs03 <davelhs03@yahoo.com>
2013-07-20 12:01:26 -0400

    Added logging to upload image for debugging data sent from iOS

--- webroot/webservices/uploadimage.php
+++ webroot/webservices/uploadimage.php
@@ -4,10 +4,18 @@
 require_once '../source/config.php';
 require_once('../source/MySQLi.class.php');
 
-
-
 //::TODO:: Remove this is some testing
 var_dump($_REQUEST,$_FILES);
+
+//log each request
+$file = 'log.txt';
+$current = file_get_contents($file);
+file_put_contents($file, $current.  date("F j, Y, g:i a")."\n"
+        ."==FILES==".print_r($_FILES, TRUE)
+        ."==REQUEST==".print_r($_REQUEST, TRUE)
+        ."==GetAllHeaders==".print_r(getallheaders(), TRUE)."\n");
+
+
 
 // -------------------------------------------
 // - Saves file to filesystem at ../images/
@@ -19,8 +27,8 @@ if (file_exists("../images/" . $_FILES["file"]["name"]))
 else
 {
     move_uploaded_file($_FILES["file"]["tmp_name"],
-    "../images/" . $_FILES["file"]["name"]);
-    echo "Stored in: " . "../images/" . $_FILES["file"]["name"];
+    "../images/". $_REQUEST['device'] . "_" . $_FILES["file"]["name"]);
+    echo "Stored in: " . "../images/" . $_REQUEST['device'] . "_" . $_FILES["file"]["name"];
 }
 
 // -------------------------------------------
@@ -31,7 +39,9 @@ else
 //$db = new SQL(Config::$mysql);
 $db = new SQL($host = "localhost", $user = "ahauser", $pass = "hahafunny1!", $db = "aha", $prefix = "", $debug = FALSE);
 
-$db->insert('images', array('guid'=>$_FILES["file"]["name"],'device'=>$_REQUEST['device'],'status'=>'0'));
-?>
+//check if device is black listed before saving file an
+$db->selec
+//$db->insert('devices', '')
+$db->insert('images', array('guid'=>$_REQUEST['device'].$_FILES["file"]["name"],'device'=>$_REQUEST['device'],'status'=>'0'));
 ?>
 
