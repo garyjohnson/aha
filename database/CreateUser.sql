88a8166fda8dcaf224b24c84400e7d78f820fc81
davelhs03 <davelhs03@yahoo.com>
2013-07-20 00:17:39 -0400

    Adding DB user creation scripts, and the image upload webservice., and ignore the images in the image directory

---
+++ database/CreateUser.sql
@@ -0,0 +1,2 @@
+CREATE USER `ahauser` IDENTIFIED BY 'hahafunny1!';
+GRANT ALL PRIVILEGES ON `Aha` . * TO 'ahauser'@'localhost' WITH GRANT OPTION ;
---
+++ webroot/webservices/uploadimage.php
@@ -0,0 +1,31 @@
+<?php
+// Take image save to file system under images and then insert into device folder 
+// and save the device to the database and the image.
+include('../source/MySQLi.class.php');
+
+
+
+//::TODO:: Remove this is some testing
+var_dump($_REQUEST,$_FILES);
+
+// -------------------------------------------
+// - Saves file to filesystem at ../images/
+// -------------------------------------------
+if (file_exists("../images/" . $_FILES["file"]["name"]))
+{
+    echo $_FILES["file"]["name"] . " already exists. ";
+}
+else
+{
+    move_uploaded_file($_FILES["file"]["tmp_name"],
+    "../images/" . $_FILES["file"]["name"]);
+    echo "Stored in: " . "../images/" . $_FILES["file"]["name"];
+}
+
+// -------------------------------------------
+// - Save metadata to the database
+// -------------------------------------------
+
+$db = new SQL($host = "localhost", $user = "ahauser", $pass = "hahafunny1!", $db = "Aha", $prefix = "", $debug = FALSE);
+var_dump($db);exit();
+?>
