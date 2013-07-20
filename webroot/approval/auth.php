569ce4bdd761f4622eff111df475983175e79788
hadidotj <hadidotj@gmail.com>
2013-07-20 01:31:15 -0400

    Created a basic login for Approval portion.

---
+++ webroot/approval/auth.php
@@ -0,0 +1,34 @@
+<?php
+class Auth {
+	const SESSION_KEY = "ahathisisawesome";
+	const LOGGED_IN_KEY = "loggedIn";
+	const USERNAME = "username";
+
+	public static function init() {
+		if(!headers_sent())
+			session_start();
+	}
+
+	public static function requireAuth() {
+		if(Auth::get(Auth::LOGGED_IN_KEY) !== TRUE || Auth::get(Auth::USERNAME) == "") {
+			header("Location: /approval/login.php");
+		}
+	}
+
+	public static function set($key, $value) {
+		$_SESSION[Auth::SESSION_KEY][$key] = $value;
+	}
+
+	public static function get($key) {
+		return $_SESSION[Auth::SESSION_KEY][$key];
+	}
+
+	public static function destroy() {
+		$_SESSION = null;
+		unset($_SESSION);
+		session_destroy();
+		session_start();
+	}
+}
+Auth::init();
+?>
---
+++ webroot/source/config.php
@@ -0,0 +1,12 @@
+<?php
+class Config {
+	public static $mysql = Array(
+		"host"=>"localhost",
+		"user"=>"root",
+		"pass"=>"",
+		"db"=>"Aha",
+		"prefix"=>"",
+		"debug"=>true
+	);
+}
+?>
