5ce44e2a0d986763fdb99e475360f6d1336f1a6e
Bill Davis <bill.davis@dxydoes.com>
2013-07-19 22:28:13 -0400

    create UploadManager class

---
+++ mobile/ios/AHAcleveland/AHAcleveland/UploadManager.m
@@ -0,0 +1,13 @@
+//
+//  UploadManager.m
+//  AHAcleveland
+//
+//  Created by Bill Davis on 7/19/13.
+//  Copyright (c) 2013 Bill Davis. All rights reserved.
+//
+
+#import "UploadManager.h"
+
+@implementation UploadManager
+
+@end
