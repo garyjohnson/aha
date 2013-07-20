f84e685c599f3c4957871c7e5cdc5ae482089e00
Gary Johnson <gary@gjtt.com>
2013-07-20 14:04:49 -0400

    Adding upload progress view

---
+++ mobile/ios/AHAcleveland/AHAcleveland/AHUploadProgressController.m
@@ -0,0 +1,20 @@
+#import "AHUploadProgressController.h"
+
+@interface AHUploadProgressController ()
+
+@end
+
+@implementation AHUploadProgressController
+
+- (id)init {
+    self = [super initWithNibName:@"AHUploadProgressView" bundle:nil];
+    if (self) {
+    }
+    return self;
+}
+
+- (NSUInteger)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+@end
