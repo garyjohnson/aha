5fcc4dd27344b726b4f6c0e77547b234b051f799
Bill Davis <bill.davis@dxydoes.com>
2013-07-19 21:54:11 -0400

    first commit

---
+++ mobile/ios/AHAcleveland/AHAcleveland/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  AHAcleveland
+//
+//  Created by Bill Davis on 7/19/13.
+//  Copyright (c) 2013 Bill Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ mobile/ios/AHAcleveland/AHAcleveland/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AHAcleveland
+//
+//  Created by Bill Davis on 7/19/13.
+//  Copyright (c) 2013 Bill Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
