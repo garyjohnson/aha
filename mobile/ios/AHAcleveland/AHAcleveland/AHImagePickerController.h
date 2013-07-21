984f078c999d77ff400fd5b6b048a63057c7a5b2
Gary Johnson <gary@gjtt.com>
2013-07-20 20:00:51 -0400

    Showing legal terms on first open

--- mobile/ios/AHAcleveland/AHAcleveland/AHImagePickerController.h
+++ mobile/ios/AHAcleveland/AHAcleveland/AHImagePickerController.h
@@ -1,6 +1,7 @@
 #import <UIKit/UIKit.h>
 #import "AHCameraOverlayDelegate.h"
+#import "AHLegaleseDelegate.h"
 
-@interface AHImagePickerController : UIViewController<UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIAlertViewDelegate, AHCameraOverlayDelegate>
+@interface AHImagePickerController : UIViewController<UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIAlertViewDelegate, AHCameraOverlayDelegate, AHLegaleseDelegate>
 
 @end
--- mobile/ios/AHAcleveland/AHAcleveland/AHLegaleseController.h
+++ mobile/ios/AHAcleveland/AHAcleveland/AHLegaleseController.h
@@ -1,7 +1,13 @@
 #import <UIKit/UIKit.h>
+#import "AHLegaleseDelegate.h"
 
 @interface AHLegaleseController : UIViewController
 
+@property (retain, readwrite, atomic) id<AHLegaleseDelegate> delegate;
+
 -(IBAction)onSurePressed:(id)sender;
+
+-(id)initWithDelegate:(id<AHLegaleseDelegate>)delegate;
+-(BOOL)hasAcceptedLegalTerms;
 
 @end
