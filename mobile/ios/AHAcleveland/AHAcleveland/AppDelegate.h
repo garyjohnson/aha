3127848abecfed2a366b976a57d555a42eebdc06
Bill Davis <bill.davis@dxydoes.com>
2013-07-20 20:09:02 -0400

    updates

--- mobile/ios/AHAcleveland/AHAcleveland/AppDelegate.h
+++ mobile/ios/AHAcleveland/AHAcleveland/AppDelegate.h
@@ -1,9 +1,13 @@
 #import <UIKit/UIKit.h>
 
+#define DEFAULTS_INSTALLATIONID @"DEFAULTS_INSTALLATIONID"
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
 @property (strong, nonatomic) UITabBarController *tabBarController;
+
+-(NSString*)installationId;
 
 @end
